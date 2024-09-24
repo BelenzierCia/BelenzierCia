fa090ab46316e950371be625eff81279108b78a9
BelenzierCia <belenzierecia@gmail.com>
2024-09-24 17:16:31 -0300

    Delete scripts directory

--- scripts/slides.js
+++
@@ -1,9 +0,0 @@
-
-let currentSlide = 0;
-
-function moveSlide(direction) {
-    const slideContainer = document.getElementById('slide-container');
-    const totalSlides = slideContainer.children.length;
-    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
-    slideContainer.style.transform = `translateX(-${currentSlide * 100 / totalSlides}%)`;
-}
